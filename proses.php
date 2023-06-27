9c369618a16fc7464c45559b5b91878c1aafc127
Muhammad Haitsam <haitsam03@gmail.com>
2023-06-27 13:08:11 +0700

    v1.3

---
+++ proses.php
@@ -0,0 +1,11 @@
+<?php 
+$hasil = 0;
+if($_POST['operasi'] == '+'){
+    $hasil = $_POST['angka1'] + $_POST['angka2'];
+} else if($_POST['operasi'] == '-'){
+} else if($_POST['operasi'] == '*'){
+} else if($_POST['operasi'] == '/'){
+
+}
+
+?>
